ab4a65a8f48753aa043bbd7b37a9a4119fb5917b
eikibriem <eikibriem@hotmail.com>
2016-11-03 13:31:11 +0000

    Ný Entity

---
+++ src/main/java/project/persistence/entities/Drug.java
@@ -0,0 +1,105 @@
+package project.persistence.entities;
+/*
+@Entity
+@Table(name = "drug") // If you want to specify a table name, you can do so here
+public class Drug {
+
+    // Declare that this attribute is the id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int dosage;
+    private int frequency;
+    private int date;
+    private String name;
+    private int activeIngr;
+    private boolean reminder;
+    private int reminderTime;
+
+
+    public Drug(int dosage, int frequency, int date, String name, int activeIngr, boolean reminder, int reminderTime) {
+        this.dosage = dosage;
+        this.frequency = frequency;
+        this.date = date;
+        this.name = name;
+        this.activeIngr = activeIngr;
+        this.reminder = reminder;
+        this.reminderTime = reminderTime;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(int dosage) {
+        this.dosage = dosage;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getActiveIngr() {
+        return activeIngr;
+    }
+
+    public void setActiveIngr(int activeIngr) {
+        this.activeIngr = activeIngr;
+    }
+
+    public boolean isReminder() {
+        return reminder;
+    }
+
+    public void setReminder(boolean reminder) {
+        this.reminder = reminder;
+    }
+
+    public int getReminderTime() {
+        return reminderTime;
+    }
+
+    public void setReminderTime(int reminderTime) {
+        this.reminderTime = reminderTime;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Postit Note[name=%s, note=%s]",
+                dosage,name);
+    }
+}
+
+
+*/
---
+++ src/main/java/project/persistence/entities/UserData.java
@@ -0,0 +1,154 @@
+
+
+package project.persistence.entities;
+
+/**
+ * Created by EiríkurAtli on 3.11.2016.
+ */
+
+/*
+@Entity
+@Table(name = "userdata") // If you want to specify a table name, you can do so here
+public class UserData {
+
+    // Declare that this attribute is the id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private int social;
+    private String address;
+    private String city;
+    private int zip;
+    private int phoneNo;    private String email;
+    private String username;
+    private String password;
+    private int accountType;
+    private List<Drug> userDrugs;
+
+
+    public UserData(String firstName, String lastName, int social, String address, String city, int zip, int phoneNo,
+                String email, String username, String password, int accountType, List<Drug> userDrugs) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.social = social;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.accountType = accountType;
+        this.userDrugs = userDrugs;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSocial() {
+        return social;
+    }
+
+    public void setSocial(int social) {
+        this.social = social;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(int accountType) {
+        this.accountType = accountType;
+    }
+
+    public List<Drug> getUserDrugs() {
+        return userDrugs;
+    }
+
+    public void setUserDrugs(List<Drug> userDrugs) {
+        this.userDrugs = userDrugs;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Postit Note[name=%s, note=%s]",
+                firstName,lastName);
+    }
+}
+
+
+*/
